57f585a757b2de8821553665cca1fcd828b2b50e
Niko Lazaris <nlazaris@hubspot.com>
2014-06-21 19:11:58 -0700

    Added typefaces

--- app/src/main/java/com/example/hackrandroid/app/ProfileFragment.java
+++ app/src/main/java/com/example/hackrandroid/app/ProfileFragment.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.hackrandroid.app.utils.DisplayUtils;
 
 
 public class ProfileFragment extends Fragment {
@@ -17,6 +20,12 @@ public class ProfileFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+    TextView name = (TextView) rootView.findViewById(R.id.profile_user_name);
+    TextView school = (TextView) rootView.findViewById(R.id.profile_user_school);
+
+    DisplayUtils.sourcesSansRegularifyTextView(name);
+    DisplayUtils.sourcesSansLightifyTextView(school);
     return rootView;
   }
 }
