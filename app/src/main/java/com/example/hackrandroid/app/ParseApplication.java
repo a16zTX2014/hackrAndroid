1073212eda1426be80aee78447679559a3df5367
Sai Avala <avala.sai@gmail.com>
2014-06-21 18:23:11 -0700

    made edittext flow in login page, added saiavala user

--- app/src/main/java/com/example/hackrandroid/app/ParseApplication.java
+++ app/src/main/java/com/example/hackrandroid/app/ParseApplication.java
@@ -18,5 +18,24 @@ public class ParseApplication extends Application {
         testObject.put("foo", "bar");
         testObject.saveInBackground();*/
 
+        /*ParseUser user = new ParseUser();
+        user.setUsername("saiavala");
+        user.setPassword("uTexas1!");
+        user.setEmail("email@example.com");
+
+// other fields can be set just like with ParseObject
+        user.put("phone", "650-253-0000");
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                }
+            }
+        });*/
+
     }
 }
